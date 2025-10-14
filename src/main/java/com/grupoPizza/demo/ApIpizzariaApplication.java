ed4f500f1af638f1899a71772559f83e76e0ad7b
RaulBrasil <raulpalheta25@gmail.com>
2025-10-14 15:55:05 -0300

    Adicionando arquivos do Spring Initializr para dar início ao projeto

---
+++ src/main/java/com/grupoPizza/demo/ApIpizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.grupoPizza.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIpizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIpizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoPizza/demo/ApIpizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoPizza.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIpizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
