4ff38f615d1f10e6a497c7bed98484fa429aa24d
Robert Filipe Miranda Costa <163329263+Lipe22md@users.noreply.github.com>
2025-11-14 08:10:23 -0300

    Refactor Pizza class with additional attributes
    Refactored Pizza class to include fields for id, name, size, price, description, and availability.

--- src/main/java/API_PIZZARIA/model/Pizza.java
+++ src/main/java/API_PIZZARIA/model/Pizza.java
@@ -1,16 +1,35 @@
-package API_PIZZARIA.model;
-
-// Classe Pizza
-/*
-Responsáveis: Vinícius, Lucas
-*/
-
-import jakarta.persistence.ManyToMany;
-
-import java.util.Set;
-
-public class Pizza {
-
-    @ManyToMany(mappedBy = "pizza")
-    Set<ItemPedido> itemPedidoSet;
-}
+package API_PIZZARIA.model; 
+ 
+import jakarta.persistence.*; 
+import lombok.AllArgsConstructor; 
+import lombok.Data; 
+import lombok.NoArgsConstructor; 
+ 
+// Responsável: VInícius, Lucas 
+ 
+@Entity 
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Table(name = "pizza") 
+public class Pizza { 
+ 
+   @Id 
+   @GeneratedValue(strategy = GenerationType.IDENTITY) 
+   private Long id; 
+ 
+   // Nome da pizza (ex: Calabresa, Frango com Catupiry) 
+   private String nome; 
+ 
+   // Tamanho (ex: pequena, média, grande) 
+   private String tamanho; 
+ 
+   // Preço da pizza 
+   private Double preco; 
+ 
+   // Sabor ou descrição adicional 
+   private String descricao; 
+ 
+   // Disponível para venda ou não 
+   private Boolean disponivel; 
+} 
