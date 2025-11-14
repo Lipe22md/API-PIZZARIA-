3748c225871ee5b1be10e72610e71646efb9d8cb
Robert Filipe Miranda Costa <163329263+Lipe22md@users.noreply.github.com>
2025-11-14 08:15:02 -0300

    Update PizzaRepository.java

--- src/main/java/API_PIZZARIA/repository/PizzaRepository.java
+++ src/main/java/API_PIZZARIA/repository/PizzaRepository.java
@@ -1,1 +1,15 @@
+package API_PIZZARIA.repository;
 
+import API_PIZZARIA.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+Responsável: Robert
+*/
+
+@Repository
+public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+
+  
+}
