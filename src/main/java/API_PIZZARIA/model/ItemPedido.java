f671c03b9091d983d9fb125392344d44dca48592
RaulBrasil <raulpalheta25@gmail.com>
2025-11-26 10:19:05 -0300

    Consertos do video enviado

--- src/main/java/API_PIZZARIA/model/ItemPedido.java
+++ src/main/java/API_PIZZARIA/model/ItemPedido.java
@@ -23,48 +23,9 @@ public class ItemPedido {
     private int quantidade;
     private double precoUnitario;
     @ManyToMany
-    Set<Pizza> pizza;
+    Set<Pedido> pedido;
 
     public double getSubtotal() {
         return precoUnitario * quantidade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public double getPrecoUnitario() {
-        return precoUnitario;
-    }
-
-    public void setPrecoUnitario(double precoUnitario) {
-        this.precoUnitario = precoUnitario;
-    }
-
-    public Set<Pizza> getPizza() {
-        return pizza;
-    }
-
-    public void setPizza(Set<Pizza> pizza) {
-        this.pizza = pizza;
-    }
-
-    public ItemPedido(Long id, int quantidade, double precoUnitario, Set<Pizza> pizza) {
-        this.id = id;
-        this.quantidade = quantidade;
-        this.precoUnitario = precoUnitario;
-        this.pizza = pizza;
     }
 }
